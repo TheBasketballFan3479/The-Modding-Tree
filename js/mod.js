2c97befc5301b81ceedf4444e438e6fa1ef9d8d9
TheBasketballFan3479 <thebasketballfan79@gmail.com>
2023-06-06 18:05:42 +0200

    1

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	name: "TheBasketballFan3479's Tree",
+	id: "tbf3479mod",
+	author: "TheBasketballFan3479",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
